4bf35cfd784c5267e9275af02d5162e534e8f20f
lilik48 <hungthong.info@gmail.com>
2015-09-25 09:11:32 +0700

    Delete file

--- Model/ViewModel/ValidateModel.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class ValidateModel
-    Public MSGVWE00001 As String ' required
-    Public MSGVWE00003 As String ' Prohibited characters
-    Public MSGVWE00015 As String ' kana full size
-    Public MSGVWE00013 As String ' calendar japanese
-    Public MSGVWE00021 As String ' number private Abnormality
-
-  
-End Class
